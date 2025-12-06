991930167e077d7009158cb5dfc493a29cae25c6
Dylan Jansen <11435343+DillaDJ@users.noreply.github.com>
2025-12-06 15:53:40 +1300

    Feat: Tag editor layout and controls

--- src/editor/tag_editor.cpp
+++ src/editor/tag_editor.cpp
@@ -1,4 +1,7 @@
 #include "tag_editor.hpp"
+#include <godot_cpp/variant/string_name.hpp>
+
+#define SNAME(m_arg) ([]() -> const StringName & { static StringName sname = StringName(m_arg, true); return sname; })()
 
 TagEditor* TagEditor::singleton = nullptr;
 
@@ -6,10 +9,112 @@ TagEditor::TagEditor() {
     singleton = this;
 
     main_vbox = memnew(VBoxContainer);
+    header_hbox = memnew(HBoxContainer);
+    add_tag_btn = memnew(Button);
+    del_tag_btn = memnew(Button);
+    del_tag_btn_hard = memnew(Button);
+    tag_tree = memnew(Tree);
+    delete_confirm = memnew(ConfirmationDialog);
 }
 
 void TagEditor::initialize() {
     set_name("Tag Editor");
 
     add_child(main_vbox);
+    
+    main_vbox->add_child(header_hbox);
+    main_vbox->set_anchors_preset(PRESET_FULL_RECT);
+
+	add_tag_btn->set_theme_type_variation("FlatMenuButton");
+	add_tag_btn->connect("pressed", callable_mp(this, &TagEditor::add_tag));
+    add_tag_btn->set_button_icon(get_theme_icon(SNAME("Add"), SNAME("EditorIcons")));
+    add_tag_btn->set_tooltip_text("Add a New Tag, if you have a tag selected, the new tag will be parented to it.");
+	header_hbox->add_child(add_tag_btn);
+    
+	del_tag_btn->set_theme_type_variation("FlatMenuButton");
+	del_tag_btn->connect("pressed", callable_mp(this, &TagEditor::prompt_soft_delete_tag));
+    del_tag_btn->set_button_icon(get_theme_icon(SNAME("Remove"), SNAME("EditorIcons")));
+    del_tag_btn->set_tooltip_text("Delete the selected tag, any child tags will be reparented to the parent of the deleted tag.");
+	header_hbox->add_child(del_tag_btn);
+    
+	del_tag_btn_hard->set_theme_type_variation("FlatMenuButton");
+	del_tag_btn_hard->connect("pressed", callable_mp(this, &TagEditor::prompt_hard_delete_tag));
+    del_tag_btn_hard->set_button_icon(get_theme_icon(SNAME("Remove"), SNAME("EditorIcons")));
+    del_tag_btn_hard->set_tooltip_text("Delete the selected tag, deleting all child tags as well.");
+	header_hbox->add_child(del_tag_btn_hard);
+
+	delete_confirm->connect("confirmed", callable_mp(this, &TagEditor::delete_selected_tag));
+    add_child(delete_confirm);
+
+    tag_tree->connect("empty_clicked", callable_mp(this, &TagEditor::empty_clicked));
+    tag_tree->set_v_size_flags(SIZE_EXPAND | SIZE_FILL);
+    tag_tree->set_hide_root(true);
+
+    root = tag_tree->create_item();
+    main_vbox->add_child(tag_tree);
+}
+
+void TagEditor::add_tag() {
+    TreeItem *selected_item = tag_tree->get_selected();
+
+    if (tag_tree->get_next_selected(selected_item) != nullptr) {
+        return;
+    }
+
+    TreeItem *parent = selected_item == nullptr ? root : selected_item;
+    TreeItem *item = tag_tree->create_item(parent);
+    
+    item->set_text(0, "Tag" + UtilityFunctions::str(parent->get_child_count()));
+}
+
+void TagEditor::prompt_soft_delete_tag() {
+    hard_delete = false;
+    prompt_delete_tag();
+}
+
+void TagEditor::prompt_hard_delete_tag() {
+    hard_delete = true;
+    prompt_delete_tag();
+}
+
+void TagEditor::prompt_delete_tag() {
+    TreeItem *selected_item = tag_tree->get_selected();
+
+    if (selected_item == nullptr) {
+        return;
+    }
+
+    delete_confirm->set_text("Delete tag: \'" + selected_item->get_text(0) + "\'" + (hard_delete ? ", as well as all children" : "") + "?");
+    delete_confirm->reset_size();
+    delete_confirm->popup_centered();
+}
+
+void TagEditor::delete_selected_tag() {
+    TreeItem *selected_item = tag_tree->get_selected();
+
+    if (selected_item == nullptr) {
+        return;
+    }
+
+    if (!hard_delete) {
+        reparent_children(selected_item);
+    }
+    
+    memdelete(selected_item);
+}
+
+void TagEditor::reparent_children(TreeItem *selected_item) {
+    TypedArray<TreeItem> children = selected_item->get_children();
+    TreeItem *parent = selected_item->get_parent();
+
+    for (size_t i = 0; i < children.size(); i++)
+    {
+        TreeItem *child = cast_to<TreeItem>(children[i]);
+        selected_item->remove_child(child);
+        parent->add_child(child);
+    }
+}
+
+void TagEditor::empty_clicked(Vector2 position, MouseButton button) {
+    tag_tree->deselect_all();
 }
--- src/editor/tag_editor.hpp
+++ src/editor/tag_editor.hpp
@@ -1,7 +1,11 @@
 #pragma once
 
 #include <godot_cpp/classes/control.hpp>
+#include <godot_cpp/classes/h_box_container.hpp>
 #include <godot_cpp/classes/v_box_container.hpp>
+#include <godot_cpp/classes/button.hpp>
+#include <godot_cpp/classes/tree.hpp>
+#include <godot_cpp/classes/confirmation_dialog.hpp>
 
 using namespace godot;
 
@@ -15,12 +19,37 @@ public:
 	static TagEditor *get_singleton() { return singleton; }
     
 	void initialize();
-
+    
 protected:
     static void _bind_methods() { }
+	
+	void add_tag();
+
+	void prompt_soft_delete_tag();
+	void prompt_hard_delete_tag();
+	void prompt_delete_tag();
+
+	void delete_selected_tag();
+
+	void reparent_children(TreeItem *selected_item);
+
+	void empty_clicked(Vector2 position, MouseButton button);
 
 private:
     static TagEditor *singleton;
 
     VBoxContainer *main_vbox;
+    HBoxContainer *header_hbox;
+
+    Button *add_tag_btn;
+    Button *del_tag_btn;
+    Button *del_tag_btn_hard;
+
+    Tree *tag_tree;
+
+    TreeItem *root;
+
+    ConfirmationDialog *delete_confirm;
+
+    bool hard_delete;
 };
